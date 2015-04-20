2f54d99e4abf15fd39734c0fdee1caba3bfa4784
Maciej Kucharek <M.Kucharek@tt.com.pl>
2015-04-20 18:33:51 +0200

    Clean up main method a bit

--- src/main/java/pl/com/tt/atp/Application.java
+++ src/main/java/pl/com/tt/atp/Application.java
@@ -12,22 +12,41 @@ import java.util.Random;
  */
 public class Application {
 
+    public static final int SPEED_UPPER_BOUND = 200;
+    public static final double LATITUDE_LOWER_BOUND = -73.0;
+    public static final double LATITUDE_UPPER_BOUND = -72.0;
+    public static final double LONGITUDE_LOWER_BOUND = 41.0;
+    public static final double LONGITUDE_UPPER_BOUND = 42.0;
+
+    public static final long POLLING_INTERVAL = 1000;
+
     public static void main(String[] args) {
+
+        // TODO: get the configuration from properties file / command line ?
+        // additional 50 lines or so..
 
         Random rand = new Random();
 
-        RandomSpeedGenerator randomSpeedGenerator = new RandomSpeedGenerator(rand, 200);
-        RandomLocationGenerator randomLocationGenerator = new RandomLocationGenerator(rand, -73.0, -72.0, 41.0, 42.0);
-        MeasurementProvider measurementProvider = new RandomMeasurementProvider(randomSpeedGenerator, randomLocationGenerator);
+        RandomSpeedGenerator randomSpeedGenerator = new RandomSpeedGenerator(rand, SPEED_UPPER_BOUND);
+        RandomLocationGenerator randomLocationGenerator = new RandomLocationGenerator(rand,
+                LATITUDE_LOWER_BOUND, LATITUDE_UPPER_BOUND, LONGITUDE_LOWER_BOUND, LONGITUDE_UPPER_BOUND);
 
+        MeasurementProvider measurementProvider =
+                new RandomMeasurementProvider(randomSpeedGenerator, randomLocationGenerator);
+
+        renderMeasurementsPeriodically(measurementProvider, POLLING_INTERVAL);
+
+    }
+
+    private static void renderMeasurementsPeriodically(MeasurementProvider measurementProvider, long period) {
         while(true) {
-            System.out.println(measurementProvider.getLatestMeasurement());
+            System.out.println("Got " + measurementProvider.getLatestMeasurement());
             try {
-                Thread.sleep(1000);
+                Thread.sleep(period);
             } catch (InterruptedException e) {
                 return;
             }
         }
-
     }
+
 }
