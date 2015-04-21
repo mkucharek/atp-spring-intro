58f2570dd94a22fc5b22ecb02914e2348e90a006
Maciej Kucharek <M.Kucharek@tt.com.pl>
2015-04-21 05:24:30 +0200

    Add CommandLineRunner

--- src/main/java/pl/com/tt/atp/Application.java
+++ src/main/java/pl/com/tt/atp/Application.java
@@ -2,9 +2,7 @@ package pl.com.tt.atp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import pl.com.tt.atp.measurement.MeasurementProvider;
 
 import java.util.Random;
 
@@ -15,27 +13,8 @@ import java.util.Random;
 @SpringBootApplication
 public class Application {
 
-    public static final long DEFAULT_POLLING_INTERVAL = 1000;
-
     public static void main(String[] args) {
-
-        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        MeasurementProvider measurementProvider = ctx.getBean(MeasurementProvider.class);
-
-        renderMeasurementsPeriodically(measurementProvider, DEFAULT_POLLING_INTERVAL);
-
-    }
-
-    private static void renderMeasurementsPeriodically(MeasurementProvider measurementProvider, long period) {
-        while(true) {
-            System.out.println("Got " + measurementProvider.getLatestMeasurement());
-            try {
-                Thread.sleep(period);
-            } catch (InterruptedException e) {
-                return;
-            }
-        }
+        SpringApplication.run(Application.class, args);
     }
 
     @Bean
